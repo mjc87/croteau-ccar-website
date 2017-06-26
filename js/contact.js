ebb3e919e86a9c45a32dc6e856f3b518dfae188e
mjc87 <mjc87@users.noreply.github.com>
2017-06-26 14:45:54 -0400

    Move contact script to contact.html

--- js/contact.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('insertEmail', []);
-app.controller('emailCtrl', function($scope, $sce) {
-	var hostname = "colorado.edu";
-	var linkcroteau = "michael.croteau" + "@" + hostname ;
-	$scope.insertEmailHere =  $sce.trustAsHtml("<a href='" + "mail" + "to:" + linkcroteau + "'>" + linkcroteau + "</a>");
-});
